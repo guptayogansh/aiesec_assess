ca25ec7cc86bf5bfc2677de445e4a0621c2890b3
guptayogansh <yogansh.11@gmail.com>
2019-09-16 16:47:56 +0530

    bug fixes for deployed app

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
 
   componentDidMount() {
 
-    fetch('http://api-staging.aiesec.org/v2/opportunities?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c')
+    fetch('https://api-staging.aiesec.org/v2/opportunities?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c')
       .then(res => res.json())
       .then((data) => {
         this.setState({ apiData: data })
